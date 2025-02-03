4f9c8006d14cafd27744d9f579824a9726b83e6f
csdresden <80101467+csdresden@users.noreply.github.com>
2025-02-03 12:14:46 +0100

    Update latest-versions.js

--- packages/devextreme-cli/src/utility/latest-versions.js
+++ packages/devextreme-cli/src/utility/latest-versions.js
@@ -1,7 +1,7 @@
 const packageJson = require('../../package.json');
 module.exports = {
     'devextreme': '24.2.3',
-    'devextreme-react': '24.2.3',
+    'devextreme-react': '24.1.9',
     'devextreme-vue': '24.2.3',
     'devextreme-cli': packageJson.version,
     'devextreme-schematics': 'latest'
